7f77fd1b0cd94e6e5ddac78ac8dfb4ff4a912f9a
Jajiso <jajiso123@gmail.com>
2021-02-03 18:30:53 +0000

    setting store to save new post and delete a post

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,19 @@ export default new Vuex.Store({
     set_posts: (state, postList) => {
       state.posts = postList
     },
+    create_post: (state, postObj) => {
+      const id = state.posts.length + 1
+      const newPost = {
+        id: id,
+        title: postObj.title,
+        body: postObj.body,
+        userId: postObj.userId
+      }
+      state.posts.push(newPost)
+    },
+    delete_post: (state, id) => {
+      state.posts = state.posts.filter((post) => post.id != id)
+    },
   },
   actions: {
     async setPostFromAPI() {
@@ -20,6 +33,12 @@ export default new Vuex.Store({
       const data = await fetchData.json()
       this.commit('set_posts', data)
     },
+    createPost(context, postObj) {
+      context.commit('create_post', postObj)
+    },
+    deletePost(context, id) {
+      context.commit('delete_post', id)
+    }
   },
   getters: {
     getPostList: state => state.posts,
